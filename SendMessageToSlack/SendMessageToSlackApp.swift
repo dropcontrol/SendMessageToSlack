29ed32227e488867afcacbdc8749af129213c5c4
hiroshi yamato <yamato@gmail.com>
2020-11-03 11:08:06 +0900

    Initial Commit

---
+++ SendMessageToSlack/SendMessageToSlackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendMessageToSlackApp.swift
+//  SendMessageToSlack
+//
+//  Created by hiroshi yamato on 2020/11/03.
+//
+
+import SwiftUI
+
+@main
+struct SendMessageToSlackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
